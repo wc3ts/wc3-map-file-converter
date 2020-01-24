ff3aed5f7b536cc86769f5357d78eeb91705bae0
AlexanderRohde <rohde.alx@gmail.com>
2020-01-24 17:19:26 +0100

    refactor trigger

--- src/trigger.ts
+++ src/trigger.ts
@@ -3,17 +3,10 @@ import { strict } from 'assert';
 import { parse } from 'ini';
 import { readFileSync } from 'fs';
 
-export interface File {
-    categoryCount: number,
-    triggerCount: number,
-    commentCount: number,
-    customScriptCount: number;
-    variableCount: number,
-    variables: Variable[],
-    elementCount: number,
-    mapName: string,
-    categories: Category[],
-    triggers: Trigger[]
+export interface Data {
+    mapName: string;
+    variables: Variable[];
+    items: Item[];
 }
 
 export interface Variable {
@@ -27,7 +20,7 @@ export interface Variable {
     parentId: number
 }
 
-export enum ElementType {
+export enum ItemType {
 	Category = 4,
 	Trigger = 8,
 	Comment = 16,
@@ -35,42 +28,42 @@ export enum ElementType {
 	Variable = 64
 }
 
-export interface Category {
-    id: number,
-    name: string,
-    parentId: number
-    isComment: boolean,
+/** Either a categrory trigger, comment, script or variable  */
+export interface Item {
+    type: ItemType;
+    id: number;
+    name: string;
+    parentId: number;
 }
 
-export interface Trigger {
-    id: number,
-    name: string,
-    parentId: number
-    kind: ElementType,
-    description: string,
-    isComment: boolean,
-    isEnabled: boolean,
-    isScript: boolean,
-    isInitiallyDisabled: boolean,
-    shouldRunOnInitialization: boolean,
-    events: Event[],
-    conditions: Condition[],
-    actions: Action[],
-    ecas: ECA[]
+export interface Category extends Item  {
+    type: ItemType.Category;
+    isComment: boolean;
 }
 
-export enum ECAType {
-    Event,
-    Condtion,
-    Action,
+export interface Trigger extends Item {
+    type: ItemType.Script | ItemType.Comment | ItemType.Trigger;
+    isComment: boolean;
+    description: string;
+    isEnabled: boolean;
+    isScript: boolean;
+    isInitiallyDisabled: boolean;
+    nodes: Node[];
 }
 
-export interface ECA {
-    kind: ECAType
+export enum NodeType {
+	Event = 0,
+	Condtion = 1,
+	Action = 2
+}
+
+/** A event, condition or action. */
+export interface Node {
+    type: NodeType
     name: string,
-    isEnabled: boolean,
+    enabled: boolean,
     parameters: Parameter[],
-    children: ECA[],
+    nodes: Node[],
     parent?: number
 }
 
@@ -83,41 +76,28 @@ export enum ParameterType {
 }
 
 export interface Parameter {
-    kind: ParameterType;
+    type: ParameterType;
     value: string;
-    hasSubParameter: boolean;
-    subParameter?: SubParameter;
-    isArray: boolean;
+    /** Set if this parameter is the return value of the given function. */
+    func?: Func;
+    /** Set if this parameter is an array. */
     index?: Parameter;
 }
 
-export enum SubParameterType {
-    Events,
-    Conditions,
-    Actions,
-    Calls,
+export enum FuncType {
+	Event = 0,
+	Condtion = 1,
+    Action = 2,
+    Call = 3,
 }
 
-export interface SubParameter {
-    kind: SubParameterType;
+export interface Func {
+    type: FuncType;
     name: string;
-    hasParameters: boolean;
     parameters: Parameter[];
 }
 
-export interface Event extends ECA {
-    kind: ECAType.Event
-}
-
-export interface Condition extends ECA {
-    kind: ECAType.Condtion
-}
-
-export interface Action extends ECA {
-    kind: ECAType.Action
-}
-
-export function read(buff: Buffer): File
+export function read(buff: Buffer): Data
 {
     const triggerData = parse(readFileSync('TriggerData.txt', 'utf-8'));
     const sections = ['TriggerEvents', 'TriggerConditions', 'TriggerActions', 'TriggerCalls'];
@@ -145,6 +125,9 @@ export function read(buff: Buffer): File
         }
     }
 
+    // param types
+    // substitute
+
     const buffer = new WarBuffer({ buff });
     const id = buffer.readFourCC();
     strict.equal(id, 'WTG!');
@@ -160,21 +143,21 @@ export function read(buff: Buffer): File
     strict.equal(buffer.readUInt32LE(), 0);
     strict.equal(buffer.readUInt32LE(), 0);
 
-    const triggerFile = {} as File;
+    const triggerFile = {} as Data;
 
-    triggerFile.categoryCount = buffer.readUInt32LE();
+    buffer.readUInt32LE(); // category count
     buffer.readBuffer(4 *  buffer.readUInt32LE());
     
-    triggerFile.triggerCount = buffer.readUInt32LE();
+    buffer.readUInt32LE(); // trigger count
     buffer.readBuffer(4 *  buffer.readUInt32LE());
 
-    triggerFile.commentCount = buffer.readUInt32LE();
+    buffer.readUInt32LE(); // comment count
     buffer.readBuffer(4 *  buffer.readUInt32LE());
 
-    triggerFile.customScriptCount = buffer.readUInt32LE();
+    buffer.readUInt32LE(); // custom script count
     buffer.readBuffer(4 *  buffer.readUInt32LE());
 
-    triggerFile.variableCount = buffer.readUInt32LE();
+    buffer.readUInt32LE(); // variable count
     buffer.readBuffer(4 *  buffer.readUInt32LE());
 
     buffer.readUInt32LE();
@@ -183,7 +166,7 @@ export function read(buff: Buffer): File
 
     triggerFile.variables = buffer.readArray(readVariable);
 
-    triggerFile.elementCount = buffer.readUInt32LE();
+    const elementCount = buffer.readUInt32LE();
 
     strict.equal(buffer.readUInt32LE(), 1);
     strict.equal(buffer.readUInt32LE(), 0);
@@ -194,37 +177,34 @@ export function read(buff: Buffer): File
     strict.equal(buffer.readUInt32LE(), 0);
     strict.equal(buffer.readInt32LE(), -1);
 
-    triggerFile.categories = [];
-    triggerFile.triggers = [];
-
-    for (let i = 0; i < (triggerFile.elementCount - 1); i++) {
-        const elementType = buffer.readUInt32LE() as ElementType;
-        switch (elementType) {
-            case ElementType.Category:
-                const category = readCategory(buffer);
-                triggerFile.categories.push(category);
-                break;
-            case ElementType.Trigger:
-            case ElementType.Comment:
-            case ElementType.Script:
-                const trigger = readTrigger(buffer, argumentCounts);
-                trigger.kind = elementType;
-                triggerFile.triggers.push(trigger);
-                break;
-            case ElementType.Variable:
-                // variables are already parsed and saved above we dont need to change anything here
-                buffer.readUInt32LE() // id
-                buffer.readStringNT() // name
-                buffer.readUInt32LE() // parent id
-                break;
-            default:
-                throw Error(`Unknown element type ${elementType}`);
-        }
-    }
+    triggerFile.items = Array
+        .from({length: elementCount - 1}, () => readItem(buffer, argumentCounts))
+        .filter(item => item.type !== ItemType.Variable);
 
     strict.equal(buffer.remaining(), 0);
 
     return triggerFile;
+}
+
+function readItem(buffer: WarBuffer, argumentCounts: Record<string, number>): Item | never {
+    const type = buffer.readUInt32LE() as ItemType;
+        switch (type) {
+            case ItemType.Category:
+                return readCategory(buffer);
+            case ItemType.Trigger:
+            case ItemType.Comment:
+            case ItemType.Script:
+                return readTrigger(buffer, argumentCounts, type);
+            case ItemType.Variable:
+                return {
+                    type: type,
+                    id: buffer.readUInt32LE(),
+                    name: buffer.readStringNT(),
+                    parentId: buffer.readUInt32LE()
+                };
+            default:
+                throw Error(`Unknown element type ${type}`);
+        }
 }
 
 function readVariable(buffer: WarBuffer): Variable {
@@ -232,7 +212,7 @@ function readVariable(buffer: WarBuffer): Variable {
     variable.name = buffer.readStringNT();
     variable.type = buffer.readStringNT();
 
-    strict.equal(buffer.readUInt32LE(), 1);
+    strict.equal(buffer.readUInt32LE(), 1); // TODO boolean?
 
     variable.isArray = buffer.readBool();
     variable.arraySize = buffer.readUInt32LE();
@@ -245,18 +225,20 @@ function readVariable(buffer: WarBuffer): Variable {
 
 function readCategory(buffer: WarBuffer): Category {
     const category = {} as Category;
+    category.type = ItemType.Category;
     category.id = buffer.readUInt32LE();
     category.name = buffer.readStringNT();
     category.isComment = buffer.readBool();
 
-    strict.equal(buffer.readUInt32LE(), 1);
+    buffer.readUInt32LE(); // TODO boolean ?
 
     category.parentId = buffer.readUInt32LE();
     return category;
 }
 
-function readTrigger(buffer: WarBuffer, argumentCounts: Record<string, number>): Trigger {
+function readTrigger(buffer: WarBuffer, argumentCounts: Record<string, number>, type: ItemType.Script | ItemType.Comment | ItemType.Trigger): Trigger {
     const trigger = {} as Trigger;
+    trigger.type = type;
     trigger.name = buffer.readStringNT();
     trigger.description = buffer.readStringNT();
     trigger.isComment = buffer.readBool();
@@ -268,44 +250,46 @@ function readTrigger(buffer: WarBuffer, argumentCounts: Record<string, number>):
     strict.equal(buffer.readBool(), false); // should run on initialization
 
     trigger.parentId = buffer.readUInt32LE();
-    trigger.ecas = Array.from({length: buffer.readUInt32LE()}, () => readECA(buffer, argumentCounts, false));
+    trigger.nodes = buffer.readArray(b => readNode(buffer, argumentCounts, false));
     return trigger;
 }
 
-function readECA(buffer: WarBuffer, argumentCounts: Record<string, number>, child: boolean): ECA {
-    const eca = {} as ECA;
-    eca.kind = buffer.readUInt32LE();
+function readNode(buffer: WarBuffer, argumentCounts: Record<string, number>, child: boolean): Node {
+    const node = {} as Node;
+    node.type = buffer.readUInt32LE();
     if(child) {
-        eca.parent = buffer.readUInt32LE();
+        node.parent = buffer.readUInt32LE();
     }
-    eca.name = buffer.readStringNT();
-    eca.isEnabled = buffer.readBool();
-    eca.parameters = Array.from({length: argumentCounts[eca.name]}, () => readParameter(buffer, argumentCounts));
-    eca.children = Array.from({length: buffer.readUInt32LE()}, () => readECA(buffer, argumentCounts, true));
-    return eca;
+    node.name = buffer.readStringNT();
+    node.enabled = buffer.readBool();
+    node.parameters = buffer.readArray(b => readParameter(b, argumentCounts), argumentCounts[node.name]);
+    node.nodes = buffer.readArray(b => readNode(b, argumentCounts, true));
+    return node;
 }
 
 function readParameter(buffer: WarBuffer, argumentCounts: Record<string, number>): Parameter {
     const parameter = {} as Parameter;
-    parameter.kind = buffer.readUInt32LE();
+    parameter.type = buffer.readUInt32LE();
     parameter.value = buffer.readStringNT();
-    parameter.hasSubParameter = buffer.readBool();
-    if(parameter.hasSubParameter) {
-        const subParameter = {} as SubParameter;
-        subParameter.kind = buffer.readUInt32LE();
-        subParameter.name = buffer.readStringNT();
-        subParameter.hasParameters = buffer.readBool();
-        if (subParameter.hasParameters) {
-            subParameter.parameters = Array.from({length: argumentCounts[subParameter.name]}, () => readParameter(buffer, argumentCounts));
-        }
-        
-        strict.equal(buffer.readUInt32LE(), 0);
-
-        parameter.subParameter = subParameter;
+    if(buffer.readBool()) { // is return value of function
+        parameter.func = readFunction(buffer, argumentCounts);
     }
-    parameter.isArray = buffer.readBool();
-    if (parameter.isArray) {
+    if (buffer.readBool()) { // is array
 		parameter.index = readParameter(buffer, argumentCounts);
-    }    
+    }
     return parameter;
+}
+
+function readFunction(buffer: WarBuffer, argumentCounts: Record<string, number>) : Func
+{
+    const func = {} as Func;
+    func.type = buffer.readUInt32LE();
+    func.name = buffer.readStringNT();
+    if (buffer.readBool()) { // has parameters
+        func.parameters = Array.from({length: argumentCounts[func.name]}, () => readParameter(buffer, argumentCounts));
+    }
+
+    strict.equal(buffer.readUInt32LE(), 0); // unknown
+    
+    return func;
 }
